d1c291e7a9ab383fb9171381456e07ce66aa45f0
kbyra <210273@student.pwr.edu.pl>
2014-03-08 14:51:22 +0100

    lab1

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+struct location
+{
+	float x;
+	float y;
+};
+
+class Circle
+{
+	location location;
+	float radius;
+
+public:
+	void set_location(struct location l)
+	{
+		location.x = l.x;
+		location.y = l.y;
+	}
+
+	::location get_location()
+	{
+		return location;
+	}
+
+	void set_radius(float r)
+	{
+		radius = r;
+	}
+
+	float get_radius()
+	{
+		return radius;
+	}
+
+	float circumference()
+	{
+		return 2 * M_PI * radius;
+	}
+
+	float area()
+	{
+		return M_PI * pow(radius, 2);
+	}
+
+};
+
+int main()
+{
+	Circle circle;
+	circle.set_location(location{ 4., 2. });
+	circle.set_radius(15.);
+
+	printf("Circle location is %.1fx%.1f.\n",
+		circle.get_location().x, circle.get_location().y);
+	printf("Circle radius is %.1f.\n",
+		circle.get_radius());
+	printf("Circle circumference is %.2f.\n",
+		circle.circumference());
+	printf("Circle area is %.2f.\n",
+		circle.area());
+
+	_getch();
+}
+
+// Wniosek 1: Zmienne obiektu struktury domyœlnie s¹ widoczne.
+// Wniosek 2: Zmienne obiektu klasy domyœlnie s¹ nie widoczne - prywatne ("cannot access private member declared in class 'Circle'").
+// Wniosek 3: Pobieramy i ustawiamy dane w klasie przy u¿yciu specjalnych publicznych metod, poniewa¿ tylko metody danej klasy
+//maj¹ dostêp do sk³adników private klasy.
